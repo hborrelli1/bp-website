4bd29edd1a06bd1c56ccea3d8635014379c51f6e
Harry Borrelli <hborrelli@Harrys-MacBook-Pro-2.local>
2021-09-27 11:41:23 -0600

    CHanges to footer component.

---
+++ components/Footer/Footer.js
@@ -0,0 +1,54 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './Footer.module.scss';
+
+// import themeConfig from '../../contentfulApi/contentful-data.preval';
+
+const Footer = () => {
+
+  return (
+    <footer className={styles.footer}>
+      <div className={styles['top-bar']}>
+        <div className={styles['logo-block']}>
+          <Image 
+            src="/assets/logos/bp-logo-icon@2x.png"
+            width="65"
+            height="65"
+            layout="fixed"
+            className={styles["img-icon"]}
+          />
+          <address className={styles.address} >
+            <h5>BORRELLI + PARTNERS</h5>
+            <p>
+              720 Vassar Street<br/>
+              Orlando, FL 32804
+            </p>
+            <p>T: <a href="tel:4074181338">407.418.1338</a></p>
+            <p>F: 407.418.1342</p>
+          </address>
+        </div>
+        <div className={styles['footer-section']}>
+          <nav>
+            <Link href="/services"><a>Services</a></Link>
+            <Link href="/our-work"><a>Our Work</a></Link>
+            <Link href="/about"><a>About</a></Link>
+            <Link href="/careers"><a>Careers</a></Link>
+            <Link href="/contact"><a>Contact</a></Link>
+          </nav>
+          <div className={styles['form-box']}>
+            <p>Form here...</p>
+            <nav>
+              <Link href="/news"><a>News</a></Link>
+              <Link href="/privacy-policy"><a>Privacy Policy</a></Link>
+            </nav>
+          </div>
+        </div>
+      </div>
+      <div className={styles['bottom-bar']} >
+        <p>&copy; 2020 Borrelli + Partners AAC000711</p>
+      </div>
+    </footer>
+  );
+}
+ 
+export default Footer;
