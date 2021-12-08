e26442a0d9588ab0175a952d126c1090f7d384c6
Harry Borrelli <harryborrelli@Harrys-MacBook-Pro.local>
2021-12-07 22:55:27 -0700

    Make Carousel Component more dynamic, separate styles into file.

--- components/Carousel/CarouselComponent.js
+++ components/Carousel/CarouselComponent.js
@@ -2,36 +2,58 @@ import Image from 'next/image';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 
-const CarouselComponent = ({ items }) => {
+const CarouselComponent = ({ items, type }) => {
   console.log('items:', items);
 
-  return (
-    <Carousel
-      showThumbs={false}
-      showArrows={true}
-      showIndicators={false}
-      infiniteLoop={true}
-      statusFormatter={(currentItem, totalCount) => (<span>0{currentItem} / 0{totalCount}</span>)}
-    >
-      {items.map(item => (
-        <div className="featured-project" key={item.id}>
-          <div className="image-col">
-            <Image 
-              src={`https:${item.thumbnail.fields.file.url}`} 
-              width="726" 
-              height="486" 
-              alt=""
-            />
+  if (type === 'projects') {
+    return (
+      <Carousel
+        showThumbs={false}
+        showArrows={true}
+        showIndicators={false}
+        infiniteLoop={true}
+        statusFormatter={(currentItem, totalCount) => (<span>0{currentItem} / 0{totalCount}</span>)}
+      >
+        {items.map(item => (
+          <div className="featured-project" key={item.id}>
+            <div className="image-col">
+              <Image 
+                src={`https:${item.thumbnail.fields.file.url}`} 
+                width="726" 
+                height="486" 
+                alt={item.thumbnail.fields.title}
+              />
+            </div>
+            <div className="content-col">
+              <span>{item.industry}</span>
+              <h3>{item.title}</h3>
+              <p>{item.excerpt}</p>
+            </div>
           </div>
-          <div className="content-col">
-            <span>{item.industry}</span>
-            <h3>{item.title}</h3>
-            <p>{item.excerpt}</p>
-          </div>
-        </div>
-      ))}
-    </Carousel>
-  )
+        ))}
+      </Carousel>
+    )
+  } else {
+    return (
+      <Carousel
+        showThumbs={false}
+        showArrows={true}
+        showIndicators={false}
+        infiniteLoop={true}
+        statusFormatter={(currentItem, totalCount) => (<span>0{currentItem} / 0{totalCount}</span>)}
+      >
+        {items.map(item => (
+          <Image 
+            src={`https:${item.fields.file.url}`} 
+            width="1109" 
+            height="624" 
+            alt={item.fields.title}
+          />
+        ))}
+      </Carousel>
+    )
+  }
+
 }
 
 export default CarouselComponent;
