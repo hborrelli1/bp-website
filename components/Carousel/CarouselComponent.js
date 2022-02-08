738e441c2b86a4fa3ea12d7c1b4e9273a4949c08
Harry Borrelli <harryborrelli@Harrys-MacBook-Pro.local>
2022-02-07 21:34:22 -0700

    Clean up code

--- components/Carousel/CarouselComponent.js
+++ components/Carousel/CarouselComponent.js
@@ -4,7 +4,6 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a lo
 import { Carousel } from 'react-responsive-carousel';
 
 const CarouselComponent = ({ items, type }) => {
-  console.log('items:', items);
 
   if (type === 'projects') {
     return (
