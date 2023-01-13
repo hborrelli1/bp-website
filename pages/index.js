c8b7936c740963fc4a5683bb71b98b1e2a3fc0c5
Harry Borrelli <hborrelli@outlook.com>
2023-01-12 18:55:39 -0700

    Progress on fix.

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export const getStaticProps = async () => {
   });
 
   const homePageData = await client.getEntries({ content_type: 'homePage' });
-  // const themeConfig = await client.getEntries({ content_type: 'themeConfig' });
+  const themeConfig = await client.getEntries({ content_type: 'themeConfig' });
   const projects = await client.getEntries({ content_type: 'projects', limit: 10 });
 
   const stringifiedHomePageData = safeJsonStringify(homePageData);
@@ -32,19 +32,18 @@ export const getStaticProps = async () => {
   return {
     props: {
       homePageData: homeData.items,
-      // themeConfig: themeConfig.items,
+      themeConfig: themeConfig.items,
       projects: projectData.items,
     },
     revalidate: 1,
   }
 }
 
-// export default function Home({homePageData, themeConfig, projects}) {
-export default function Home({homePageData, projects}) {
+export default function Home({homePageData, themeConfig, projects}) {
   const {fields} = homePageData[0];
   const [navHeight, setNavHeight] = useState(60);
   const [heroHeight, setHeroHeight] = useState(0);
-
+console.log({themeConfig})
   useEffect(() => {
     // Set navbar height on load
     const navElement = document.getElementById('Navbar');
@@ -62,9 +61,9 @@ export default function Home({homePageData, projects}) {
     height: '100vh',
   }
 
-  // const backgroundSectionStyles = {
-  //   backgroundImage: `url(https:${themeConfig[0].fields.backgroundTexture.fields.file.url})`,
-  // }
+  const backgroundSectionStyles = {
+    backgroundImage: `url(https:${themeConfig[0].fields.backgroundTexture.fields.file.url})`,
+  }
 
   const featuredProjectItems = fields.featuredProjects.reduce((acc, item, index) => {
     acc[index] = {
@@ -107,8 +106,7 @@ export default function Home({homePageData, projects}) {
             </div>
           </button>
         </div>
-        {/* <div className="services" style={backgroundSectionStyles}> */}
-        <div className="services" >
+        <div className="services" style={backgroundSectionStyles}>
           <div className="content">
             <div className="content-column">
               <h2>{fields.ourServicesTitle}</h2>
