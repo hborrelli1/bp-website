91b6059689e25a721029bad8d05f637dab2003ec
Harry Borrelli <hborrelli@outlook.com>
2023-07-26 20:52:09 -0600

    Attempt to improve data fetching. (#40)
    * Attempt to improve data fetching.
    
    * REmove script from package.json

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,9 +1,9 @@
 import {createClient} from 'contentful';
-import TwoColumnHeader from '../../components/TwoColumnHeader/TwoColumnHeader';
-import FooterCta from '../../components/FooterCta/FooterCta';
+import TwoColumnHeaderGQL from '../../components/TwoColumnHeaderGQL/TwoColumnHeaderGQL';
+import FooterCtaGQL from '../../components/FooterCtaGQL/FooterCtaGQL';
 import Image from 'next/image';
 import Link from 'next/link';
-import BlogCard from '../../components/BlogCard';
+import BlogCardGQL from '../../components/BlogCardGQL';
 import { useState } from 'react';
 import safeJsonStringify from 'safe-json-stringify';
 import moment from 'moment';
@@ -11,28 +11,92 @@ import moment from 'moment';
 // Runs at build time
 // Used to fetch data from Blog section.
 export const getStaticProps = async () => {
-  // const res = await fetch(/** contentful api here */);
-  // const data = await res.json();
+  const space = process.env.CONTENTFUL_SPACE_ID;
+  const accessToken = process.env.CONTENTFUL_ACCESS_KEY;
 
-  const client = createClient({
-    space: process.env.CONTENTFUL_SPACE_ID,
-    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
-  });
+  const res = await fetch(
+    `https://graphql.contentful.com/content/v1/spaces/${space}`,
+    {
+      method: 'POST', // GraphQL *always* uses POST requests!
+      headers: {
+        'content-type': 'application/json',
+        authorization: `Bearer ${accessToken}`, // add our access token header
+      },
+      // send the query we wrote in GraphiQL as a string
+      body: JSON.stringify({
+        // all requests start with "query: ", so we'll stringify that for convenience
+        query: `
+        {
+          newsPageCollection(limit:1) {
+            items {
+              pageTitle
+              pageDescription
+              backgroundImage {
+                url
+              }
+              featuredNews {
+                blogTitle
+                shortSummary
+                thumbnailImage {
+                  url
+                  width
+                  height
+                }
+                slug
+              }
+              footerCta {
+                copy 
+                ctaText
+                ctaLink
+                backgroundImage {
+                  url
+                }
+              }
+              newsPostsCollection(where:{sys:{id_exists:true}}) {
+                items {
+                  shortSummary
+                  thumbnailImage {
+                    url
+                  }
+                }
+              }
+            }
+          }
+        }
+      `,
+      }),
+    },
+  );
 
-  const newsRes = await client.getEntries({ content_type: 'newsPage', include: 2});
-  const stringifiedNews = safeJsonStringify(newsRes);
-
-  const newsData = JSON.parse(stringifiedNews);
+  const data = await res.json()
+  // const data2 = await res2.json()
 
   return {
     props: {
-      newsPage: newsData.items[0],
+    	pageData: data.data.newsPageCollection.items,
+      // featuredProjects: data2.data.projectsCollection.items,
     },
-    revalidate: 1,
   }
+
+  // const client = createClient({
+  //   space: process.env.CONTENTFUL_SPACE_ID,
+  //   accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+  // });
+
+  // const newsRes = await client.getEntries({ content_type: 'newsPage'});
+  // const stringifiedNews = safeJsonStringify(newsRes);
+
+  // const newsData = JSON.parse(stringifiedNews);
+
+  // return {
+  //   props: {
+  //     newsPage: newsData.items[0],
+  //   },
+  //   revalidate: 1,
+  // }
 }
 
-const News = ({ newsPage }) => {
+const News = ({ pageData }) => {
   const {
     backgroundImage,
     featuredNews,
@@ -40,12 +104,12 @@ const News = ({ newsPage }) => {
     pageDescription,
     pageTitle,
     date,
-    newsPosts
-  } = newsPage.fields;
+    newsPostsCollection
+  } = pageData[0]
 
   const [itemsToDisplay, setItemsToDisplay] = useState(6);
 
-  const blogItemsDisplay = newsPosts?.map((blog, index) => <BlogCard key={index} blog={blog} type={'news'} />);
+  const blogItemsDisplay = newsPostsCollection.items?.map((blog, index) => <BlogCardGQL key={index} blog={blog} type={'news'} />);
 
   const increaseItemsToDisplay = () => {
     setItemsToDisplay(itemsToDisplay + 6);
@@ -53,7 +117,7 @@ const News = ({ newsPage }) => {
 
   return (
     <article className="news-list">
-      <TwoColumnHeader 
+      <TwoColumnHeaderGQL 
         title={pageTitle}
         copy={pageDescription}
         image={backgroundImage}
@@ -63,17 +127,17 @@ const News = ({ newsPage }) => {
           <h3 className="sub-title">Featured News:</h3>
           <div className="img-col">
             <Image 
-              src={`https:${featuredNews.fields.thumbnailImage.fields.file.url}`} 
-              width={featuredNews.fields.thumbnailImage.fields.file.details.image.width}
-              height={featuredNews.fields.thumbnailImage.fields.file.details.image.height}
-              alt={featuredNews.fields.blogTitle}
+              src={featuredNews.thumbnailImage.url} 
+              width={featuredNews.thumbnailImage.width}
+              height={featuredNews.thumbnailImage.height}
+              alt={featuredNews.blogTitle}
             />
           </div>
           <div className="content-col">
-            <h2>{featuredNews.fields.blogTitle}</h2>
+            <h2>{featuredNews.blogTitle}</h2>
             <h5 className="date">{moment(date).format('MMMM Do YYYY')}</h5>
-            <p className="body-copy">{featuredNews.fields.shortSummary}</p>
-            <Link href={`/news/${featuredNews.fields.slug}`}>
+            <p className="body-copy">{featuredNews.shortSummary}</p>
+            <Link href={`/news/${featuredNews.slug}`}>
               <a className="blog-link">Keep reading +</a>
             </Link>
           </div>
@@ -107,12 +171,12 @@ const News = ({ newsPage }) => {
         </div>
       </section>
 
-      <FooterCta 
+      <FooterCtaGQL 
         ctaData={{
-          copy: footerCta.fields.copy,
-          buttonText: footerCta.fields.ctaText,
-          buttonUrl: footerCta.fields.ctaLink,
-          backgroundImage: footerCta.fields.backgroundImage
+          copy: footerCta.copy,
+          buttonText: footerCta.ctaText,
+          buttonUrl: footerCta.ctaLink,
+          backgroundImage: footerCta.backgroundImage
         }}
       />
     </article>
