c21b56fc1599b6bd67f867fa32cb4d0138f33059
Harry Borrelli <hborrelli@outlook.com>
2023-08-08 16:54:08 -0600

    Fix footer and remove fax

--- components/BlogCard.js
+++ components/BlogCard.js
@@ -14,44 +14,40 @@ const BlogCard = ({blog, type = "news"}) => {
   const postTitle = type === "news" ? blogTitle : projectTitle;
   
   return (
-    // <div className='blog-card' onClick={() => router.push(`/${type}/${slug}`)}>
-      <Link href={`/${type}/${slug}`}>
-        <a className='blog-card'> 
-          <div className="img-wrap">
-            <div className="img-hover-circle">
-              <div className="icon">
-                <Image 
-                  src="/assets/icons/circle-with-plus@2x.png"
-                  width="40px"
-                  height="40px"
-                  alt={`${postTitle || ''} Thumbnail Image.`}
-                  />
-              </div>
-            </div>
-            <div className="img-hover-filter-bg"></div>
-            <div className="img-el">
-              {thumbnailImage && thumbnailImage.fields && (
-                <Image  
-                  src={`https:${thumbnailImage.fields.file.url}`}
-                  // width={thumbnailImage.fields.file.details.image.width}
-                  // height={thumbnailImage.fields.file.details.image.height}
-                  alt={postTitle || ''}
-                  className="img"
-                  layout="fill"
-                  fill={true}
+    <Link href={`/${type}/${slug}`}>
+      <a className='blog-card'> 
+        <div className="img-wrap">
+          <div className="img-hover-circle">
+            <div className="icon">
+              <Image 
+                src="/assets/icons/circle-with-plus@2x.png"
+                width="40px"
+                height="40px"
+                alt={`${postTitle || ''} Thumbnail Image.`}
                 />
-              )}
             </div>
           </div>
-          <div className="info">
-            <h4>{postTitle || ''}</h4>
-            {date && <h5 className="date">{moment(date).format('MMMM Do YYYY')}</h5>}
-            <p className="body-copy">{shortSummaryText || ''}</p>
-          <p className="link">Keep Reading +</p>
+          <div className="img-hover-filter-bg"></div>
+          <div className="img-el">
+            {thumbnailImage && thumbnailImage.fields && (
+              <Image  
+                src={`https:${thumbnailImage.fields.file.url}`}
+                alt={postTitle || ''}
+                className="img"
+                layout="fill"
+                fill={true}
+              />
+            )}
           </div>
-        </a>
-      </Link>
-    // </div>
+        </div>
+        <div className="info">
+          <h4>{postTitle || ''}</h4>
+          {date && <h5 className="date">{moment(date).format('MMMM Do YYYY')}</h5>}
+          <p className="body-copy">{shortSummaryText || ''}</p>
+        <p className="link">Keep Reading +</p>
+        </div>
+      </a>
+    </Link>
   );
 }
  
